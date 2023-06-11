86ff2069400198dfc917f0383c1c769eada4fff2
Hansika - <iamkislayraj@gmail.com>
2023-06-11 23:51:48 +0530

    New file added

---
+++ Recursion/src/com/learn_2/code/power_of_number.java
@@ -0,0 +1,11 @@
+package com.learn_2.code;
+
+public class power_of_number {
+	static int pom(int num,int ex) {
+		if(ex==0) {
+			return 0;
+		}
+		int result=pom(num,ex-1);
+		
+	}
+}
--- Recursion/src/com/learn_2/code/practise_stack.java
+++
@@ -1,5 +0,0 @@
-package com.learn_2.code;
-
-public class practise_stack {
-	
-}
